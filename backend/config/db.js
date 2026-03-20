f17124b243172264bdfedfb8520942a31da20d21
swathi k <swathik.ece.rymec@gmail.com>
2026-03-20 20:14:14 +0530

    Add DB_PORT to database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD || '',
     {
         host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
         dialect: 'mysql',
         logging: false, // disable logging queries to terminal
     }
